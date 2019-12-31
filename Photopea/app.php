8647cb414fdae86376215347e08d65764be2cc00
zhtengw <atenzd@gmail.com>
2019-12-31 18:02:04 +0800

    Photopea: restructure files, fixed cannot show filename on photopea tab

--- Photopea/app.php
+++ Photopea/app.php
@@ -25,16 +25,14 @@ class PhotopeaPlugin extends PluginBase {
         }
         $fileName = get_path_this($path);
         $fileExt = get_path_ext($path);
-        $fileUrl = $this->pluginHost.'php/handler.php?act=sent&path='.rawurlencode($path);
+        $fileUrl = $this->pluginHost.'php/handler.php?act=sent&path='.$path;
         $saveUrl = $this->pluginHost.'php/handler.php?act=save&path='.rawurlencode($path);
         $fullUri = '{"files":["'.$fileUrl.'"],"resources":[],"server":{"version":1,"url":"'.$saveUrl.'","formats":["'.$fileExt.'"]},"environment":{},"script":""}';
-        
-        $http_type = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') || (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https')) ? 'https://' : 'http://';
-        $pre = $http_type.$_SERVER['HTTP_HOST'].substr(__DIR__,strlen($_SERVER["DOCUMENT_ROOT"]));
+
 		$config = $this->getConfig();
 
         //header('Location:https://www.photopea.com/#'.urlencode($fullUri));
         //国内访问速度问题，故本地化
-        header('Location:'.$pre.'/ps/#'.($fullUri));
+        header('Location:'.$this->pluginHost.'/static/photopea/#'.($fullUri));
     }
 }
