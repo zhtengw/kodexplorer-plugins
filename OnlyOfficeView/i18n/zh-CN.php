8dbbe316ca7432627421a35726192b892bfd1e24
zhtengw <atenzd@gmail.com>
2019-12-31 09:19:55 +0800

    Add new plugin: OnlyOfficeView

---
+++ OnlyOfficeView/i18n/zh-CN.php
@@ -0,0 +1,13 @@
+<?php
+return array(
+	"OnlyOfficeView.meta.name"				=> "OnlyOffice 在线预览",
+	"OnlyOfficeView.meta.title"				=> "OnlyOffice 在线预览",
+	"OnlyOfficeView.meta.desc"				=> "在线查看Office文档",
+	"OnlyOfficeView.Config.apiServer"		=> "服务器接口",
+	"OnlyOfficeView.Config.apiServerDesc"	=> "
+	    <div class='can-select pt-10'>
+	    OnlyOfficeView API的地址必须要与部署KodExplorer的地址是相同的协议；<br>
+	    如果KodExplorer是http站点，则OnlyOfficeView API的http地址不能为空；<br>
+	    如果KodExplorer是https站点，则OnlyOfficeView API的https地址不能为空；<br>
+	    另外，如果端口不是80或443，需要指明端口号。</div>"
+);
