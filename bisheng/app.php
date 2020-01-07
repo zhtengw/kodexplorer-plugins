2a07d22b39e358057494d65d2a8578533fe2be77
zhtengw <atenzd@gmail.com>
2020-01-08 02:02:28 +0800

    New plugin: bisheng

---
+++ bisheng/app.php
@@ -0,0 +1,80 @@
+<?php
+
+class bishengPlugin extends PluginBase {
+    function __construct() {
+        parent::__construct();
+    }
+    public function regist() {
+        $this->hookRegist(array(
+            'user.commonJs.insert' => 'bishengPlugin.echoJs'
+        ));
+    }
+    public function echoJs() {
+        $this->echoFile('static/main.js');
+
+    }
+    public function index() {
+        $path = $this->filePath($this->in['path']);
+        $localFile = $this->pluginLocalFile($this->in['path']);
+        $fileUrl = $this->filePathLinkOut($this->in['path']);
+        $fileName = $this->fileInfo['name'];
+        $fileExt = get_path_ext($this->fileInfo['name']);
+
+        $config = $this->getConfig();
+        $apiServer = $config['apiServer'].'/apps/editor/openPreview?data=';
+        $options = array(
+            'doc' => array(
+                'docId' => md5_file($localFile),
+                'title' => $fileName,
+                //'mime_type' => mime_content_type($fileName),
+                'fetchUrl' => $fileUrl,
+                'callback' => '',),
+            'user' => array(
+                'uid' => Session::get('kodUser.userID'),
+                'nickName' => Session::get('kodUser.nickName').' ('.Session::get('kodUser.name').')',
+                'avatar' => Session::get('kodUser.avatar'),
+                'privilege' => array('FILE_READ'),
+                )
+            );
+        $timestamp = filemtime($localFile);
+
+        //kodbox默认最低权限是canView可预览
+        //可读权限检测，可读则可下载及打印
+        if (Action("explorer.auth")->fileCanRead($path)) {
+            array_push($options['user']['privilege'],'FILE_DOWNLOAD', 'FILE_PRINT');
+        }
+
+        //可写权限检测
+        if (Action("explorer.auth")->fileCanWrite($path)) {
+            array_push($options['user']['privilege'],'FILE_WRITE');
+            $options['doc']['docId'] = md5($localFile.$timestamp);
+            $options['doc']['callback'] = $this->pluginApi.'save&cache='.$localFile.'&path='.$path.'&api='.$config['apiServer'];
+            $apiServer = $config['apiServer'].'/apps/editor/openEditor?data=';
+        }
+        
+        if (strlen($apiServer) > 0) {
+            //print_r(json_encode($options));
+            header('Location:'.$apiServer.base64_encode(json_encode($options)));
+        } else {
+            show_tips("bisheng Document Server is not available.");
+        }
+    }
+    public function save() {
+        if (($body_stream = file_get_contents("php://input")) === FALSE) {
+            echo "Bad Request";
+        }
+        $data = json_decode($body_stream, TRUE);
+        if (!$data['data']["unchanged"]) {
+            if (($new_office_content = file_get_contents($_GET['api'].$data['data']["docURL"])) === FALSE) {
+                echo "Bad Response";
+            } else {
+                if(file_put_contents($_GET['cache'], $new_office_content, LOCK_EX)){
+                    $this->pluginCacheFileSet($_GET['path'], file_get_contents($_GET['cache']));
+                    del_file($_GET['cache']);
+                }
+
+            }
+        }
+        echo "{\"error\":0}";
+    }
+}
---
+++ bisheng/i18n/zh-CN.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+	"bisheng.meta.name"				=> "毕升Office",
+	"bisheng.meta.title"			=> "毕升Office在线编辑器",
+	"bisheng.meta.desc"				=> "在线查看和编辑Office文档",
+	"bisheng.Config.apiServer"		=> "服务器接口",
+	"bisheng.Config.apiServerDesc"	=> "
+	    <div class='can-select pt-10'>
+	    毕升Office API的地址，要求和当前打开KodExplorer的地址可以互相访问，否则将无法保存文档。<br/>
+	    如果端口不是80或443，需要指明端口号。<br/>
+	    部署方式请参考<a href='https://www.bishengoffice.com/apps/blog/posts/install.html' target='_blank'>毕升官方文档</a>。</div>"
+);
