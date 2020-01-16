2e23d01d59649a8d4dbda01760bd65665438f39c
zhtengw <atenzd@gmail.com>
2020-01-16 18:26:09 +0800

    bisheng: cleanup

--- bisheng/php/handler.php
+++ bisheng/php/handler.php
@@ -22,8 +22,6 @@ function saveBack() {
         echo "Bad Request";
     }
     $data = json_decode($body_stream, TRUE);
-    //file_put_contents('/mnt/dump/public/data.txt',$body_stream);
-    //file_put_contents('/mnt/dump/public/docURL.txt',$_GET['api'].$data['data']["docURL"]);
     if (!$data['data']["unchanged"]){
         if (($new_office_content = file_get_contents($_GET['api'].$data['data']["docURL"]))===FALSE){
             echo "Bad Response";
