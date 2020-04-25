32a3186b5f38095648474e306427d5f7976dbe1a
zhtengw <atenzd@gmail.com>
2020-04-25 22:08:32 +0800

    move to kodbox dir

--- Photopea/i18n/en.php
+++
@@ -1,6 +0,0 @@
-<?php
-return array(
-	"Photopea.meta.name"				=> "Photopea | Online PS",
-	"Photopea.meta.title"				=> "Photopea | Online PS",
-	"Photopea.meta.desc"				=> "Online Photo Editor, PhotoShop online",
-);
